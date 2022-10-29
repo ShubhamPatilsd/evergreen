cb1d393de6fd8f89b1d09634c6a38be1973d79ac
Shubham <shubhampatilsd@gmail.com>
2022-10-29 15:58:04 -0700

    bonaparting

--- pages/auth/onboarding.tsx
+++ pages/auth/onboarding.tsx
@@ -1,3 +1,8 @@
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import prisma from "../../lib/prismadb";
+
 export default function Home() {
   return (
     <div className="space-y-10 p-10">
@@ -41,3 +46,31 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+
+  if (session) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email || "",
+      },
+    });
+
+    if (user?.onboarded) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+  }
+  return {
+    props: {},
+  };
+};
