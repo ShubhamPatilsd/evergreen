8c0c0c8687df05dcf655e58c5dc43d2c6055b3dd
Shubham <shubhampatilsd@gmail.com>
2022-10-29 14:57:59 -0700

    additional nextauth set up

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,13 +2,25 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
+import type { NextAuthOptions } from "next-auth";
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  pages: {
+    newUser: "/auth/onboarding",
+  },
+};
+
+export default NextAuth(authOptions);
---
+++ prisma/migrations/20221029215712_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "onboarded" BOOL NOT NULL DEFAULT false;
