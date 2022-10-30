8b9773f04bbc1c6e41c5229ddc73014f4868019f
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-10-29 19:03:40 -0700

    navigation with create post button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,19 @@
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { buttonClasses } from "../classes/button";
 
 export const Navbar = () => {
   const { data: session, status } = useSession();
+  const router = useRouter();
+
   return (
     <div className="flex items-center justify-between px-4 py-2">
       <h1 className="text-3xl">🌲</h1>
       <div className="flex items-center space-x-4">
-        <button className="rounded-lg bg-accent px-4 py-1 font-bold text-white transition hover:bg-accent-darker">
+        <button
+          className="rounded-lg bg-accent px-4 py-1 font-bold text-white transition hover:bg-accent-darker"
+          onClick={() => router.push("/create/post")}
+        >
           Create a post
         </button>
         <img
