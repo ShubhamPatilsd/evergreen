b68dce913dd8ada8e30beeaca3519e0c69e05dd0
Shubham <shubhampatilsd@gmail.com>
2022-10-30 00:05:33 -0700

    primsa

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,6 +10,9 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      httpOptions: {
+        timeout: 60000,
+      },
     }),
   ],
   callbacks: {
